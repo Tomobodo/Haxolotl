bd51150009b783f4a0e8dc85e253b056e20264bb
Thomas BAUDON <baudon.thomas@gmail.com>
2014-02-11 18:13:28 +0100

    bunnies sample

---
+++ samples/bunniesEverywhere/src/Bunny.hx
@@ -0,0 +1,83 @@
+package ;
+import haxolotl.core.Texture;
+import haxolotl.core.TextureRegion;
+import haxolotl.display.Image;
+
+/**
+ * ...
+ * @author Thomas B
+ */
+class Bunny extends Image
+{
+	var vitX : Float;
+	var vitY : Float;
+	
+	var time : Int;
+
+	public function new() 
+	{
+		var atlas = Main.atlas;
+		
+		super(atlas.get("bunny"));
+		
+		vitX = Math.random() * 15 + 8;
+		vitY = Math.random() * - 2 - 4;
+		
+		x = 40 - width/2;
+		y = 110 - height/2;
+		
+		alpha = 1;
+		
+		color.r = Math.random();
+		color.g = Math.random();
+		color.b = Math.random();
+		alpha = 0;
+		
+		time = cast Math.random() * 500;
+		
+		ENTER_FRAME.add(onEnterFrame);
+		
+		HOVERED.add(onHoveredd);
+	}
+	
+	function onHoveredd(a) 
+	{
+		if (parent != null)
+			parent.remove(this);
+	}
+	
+	public function onEnterFrame() 
+	{
+		x += vitX;
+		
+		if (alpha < 1)
+			alpha += 0.02;
+		
+		if (x + width > stage.width)
+		{
+			x = stage.width - width;
+			vitX *= -1;
+		}else if (x < 0)
+		{
+			x = 0;
+			vitX *= -1;
+		}
+		
+		vitY += 0.3;
+		y += vitY;
+		
+		if (y + height > stage.height)
+		{
+			y = stage.height - height;
+			vitY *= -0.9;
+		}
+		
+		time--;
+		
+		if (time <= 0)
+		{
+			time = cast Math.random() * 500;
+			vitY += Math.random() * -6;
+		}
+	}
+}
---
+++ samples/bunniesEverywhere/src/Main.hx
@@ -0,0 +1,80 @@
+package ;
+
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.Lib;
+import haxolotl.core.Engine;
+import haxolotl.core.InteractiveObject;
+import haxolotl.core.Stage;
+import haxolotl.core.Texture;
+import haxolotl.core.TextureAtlas;
+import haxolotl.display.Image;
+
+/**
+ * ...
+ * @author Thomas B
+ */
+
+class Main extends Sprite 
+{
+	
+	public static var atlas : TextureAtlas;
+	
+	var engine : Engine;
+	
+	var sampleStage : Stage;
+	
+	var mouth : Image;
+	
+	var mouseDown : Bool;
+	
+	public function new()
+	{
+		super();
+		addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+	}
+	
+	private function onAddedToStage(e:Event):Void 
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+		
+		engine = new Engine(stage);
+		
+		atlas = new TextureAtlas(Texture.get("img/atlas.png"), "img/atlas.xml");
+		
+		sampleStage = new Stage();
+		sampleStage.ENTER_FRAME.add(stageEnterFrame);
+		engine.add(sampleStage);
+		
+		mouth = new Image(atlas.get("lol2"));
+		sampleStage.add(mouth);
+		
+		mouth.PRESSED.add(mouthPressed);
+		mouth.RELEASED.add(mouthReleased);
+		mouth.LEAVED.add(mouthReleased);
+	}
+	
+	function stageEnterFrame() 
+	{
+		if (mouseDown)
+			for (i in 0 ... 20)
+				sampleStage.add(new Bunny());
+	}
+	
+	function mouthReleased(target : InteractiveObject) 
+	{
+		mouth.texture = atlas.get("lol2");
+		mouseDown = false;
+	}
+	
+	function mouthPressed(target : InteractiveObject) 
+	{
+		mouth.texture = atlas.get("lol");
+		mouseDown = true;
+	}
+
+	public static function main() 
+	{
+		Lib.current.addChild(new Main());
+	}
+}
